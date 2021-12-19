fca1ac1e6ee38614459ef1ba5e02a40b613a9ebc
moku6626 <mokutani0626@gmail.com>
2021-12-19 20:53:14 +0900

    commit 1st

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function() {
+    return view('home');
+});
+
+//登録画面へのルーティング
+Route::get('/register', [App\Http\Controllers\RegisterController::class, 'create'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/register', [App\Http\Controllers\RegisterController::class, 'store'])
+    ->middleware('guest');
+
+//ログイン機能のルーティング
+Route::get('/login', [App\Http\Controllers\LoginController::class, 'index'])
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', [App\Http\Controllers\LoginController::class, 'authenticate'])
+    ->middleware('guest');
+
+//ログアウト
+Route::get('/logout', [App\Http\Controllers\LoginController::class, 'logout'])
+    ->middleware('auth')
+    ->name('logout');
